a7aac1478361d8e82b58f6bb6062565a132b3911
c.mariselvan <mari20041999@gmail.com>
2022-09-19 14:59:24 +0530

    add files

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ var number = parseInt(prompt('Enter a given number'));
 var temp = number;
 var sum = 0;
 var lastDigit;
+console.log('Given number is '+number);
 while (temp) {
     lastDigit = temp % 10;
     sum=sum+lastDigit;
     temp=(temp-lastDigit)/10;
 
 }
-console.log('Sum of given number');
+console.log('Sum of given number is');
 console.log(sum);
